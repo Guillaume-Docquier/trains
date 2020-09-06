4e38515c1960cdc8c52431ede1f4d8cbfe9f161d
Guillaume Docquier <guillaumedocquier@hotmail.com>
2020-09-05 20:14:23 -0400

    Added test to validate that Searching can beat Heuristics

--- Trains.Tests/UnitTests.cs
+++ Trains.Tests/UnitTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 using Trains.models;
+using Trains.Solvers;
 
 namespace Trains.Tests
 {
@@ -18,7 +20,7 @@ namespace Trains.Tests
         [Test]
         [TestCase(new[] { "0000ACDGC", "0000000DG" }, 'C', "A,1,2;C,1,0;DG,1,2;C,1,0")]
         [TestCase(new[] { "0000AGCAG", "00DCACGDG" }, 'C', "AG,1,2;C,1,0;AGD,2,1;C,2,0;A,2,1;C,2,0")]
-        public void TestExampleCase1(string[] trainLines, char destination, string expected)
+        public void TrainStarter_Finds_Good_Solutions(string[] trainLines, char destination, string expected)
         {
             var solution = this._trainStarter.Start(trainLines, destination);
 
@@ -26,6 +28,16 @@ namespace Trains.Tests
             var expectedCost = Solution.GetCost(expected);
 
             Assert.LessOrEqual(cost, expectedCost);
+        }
+        
+        [Test]
+        [TestCase(new[] { "0000AGCAG", "00DCACGDG" }, 'C')]
+        public void SearchingSolver_Can_Find_Better_Solutions_Than_HeuristicsSolver(string[] trainLines, char destination)
+        {
+            var heuristicsSolution = HeuristicsSolver.Solve(trainLines, destination);
+            var searchingSolution = SearchingSolver.Solve(trainLines, destination, heuristicsSolution).Last(solution => !string.IsNullOrEmpty(solution));
+
+            Assert.Less(Solution.GetCost(searchingSolution), Solution.GetCost(heuristicsSolution));
         }
     }   
 }
